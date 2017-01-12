57d54c20809f23a1f27c3f41677971125012747d
Namratha Ravi <namr247@gmail.com>
2017-01-12 19:28:53 +0530

    Add files via upload

---
+++ finJson.js
@@ -0,0 +1,253 @@
+var finJson=
+{
+	
+	phrases: {
+		"beginsWith":{
+					"english" :" string 'w' begins with _left_ ",
+					"kannada" :" 'w' _left_ ಇಂದ ಶುರುವಾಗುತ್ತದೆ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" स्ट्रिंग  'w' _left_ से शुरू होना चाहिए "
+	                 },
+		"endsWith":{
+					"english" :" string 'w' ends with _left_ ",
+					"kannada" :" 'w' _left_ ಇಂದ ಅಂತ್ಯಗೊಳ್ಳುತ್ತದೆ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" स्ट्रिंग  'w' _left_ से समाप्त होना चाहिए "
+			       },
+		"contains":{
+					"english" :" string 'w' contains _left_ ",
+					"kannada" :" 'w' _left_ ಅನ್ನು  ಹೊಂದಿದೆ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" स्ट्रिंग  'w' में _left_ शामिल है "
+				   },
+        "atPos":{
+					"english" :" character at the position _left_ in string 'w' ",
+					"kannada" :" 'w' ವಿನಾ _left_ನೇ ಸ್ಥಾನದಲ್ಲಿರುವ  ಅಕ್ಷರಶಃ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" स्ट्रिंग  'w' में संख्या _left_ के स्थान पर अक्षर "
+				},
+		"fromEnd":{
+					"english" :" character in the position _left_ from the end of the string 'w' ",
+					"kannada" :" 'w'ವಿನಾ ಕೊನೆಯಿಂದ _left_ನೇ ಸ್ಥಾನದಲ್ಲಿರುವ  ಅಕ್ಷರಶಃ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" स्ट्रिंग  'w' में  अंत से  संख्या _left_ के स्थान पर अक्षर "
+				  },
+		"num":{
+					"english" :" the number of times _left_ occurs in string 'w' ",
+					"kannada" :" _left_ 'w' ನಲ್ಲಿ ಸಂಭವಿಸುವ ಎಣಿಕೆ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" स्ट्रिंग  'w' में _left_ के घटनाओं की संख्या "
+	          },
+		"instr":{
+					"english" :" _left_ occurs in string 'w' ",
+					"kannada" :" _left_ 'w' ನಲ್ಲಿ ಬರುತ್ತದೆ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" _left_ स्ट्रिंग  'w' में शामिल है "
+	            },
+		"exactly":{
+					"english" :" _right_ appears in string 'w' exactly _left_ time/s ",
+					"kannada" :" _right_ 'w' ನಲ್ಲಿ _left_ ಸಾರಿ ಮಾತ್ರ  ಬರುತ್ತದೆ ",
+					"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" _right_ स्ट्रिंग  'w' में केवल _left_ बार आता है "
+	              },
+		"atMost":{
+					"english" :" the occurance of _right_ in string 'w' is atmost _left_ time/s ",
+					"kannada" :" 'w' ನಲ್ಲಿ _right_ ದರ ಸಂಭವ ಹೆಚ್ಚೆಂದರೆ _left_ ಬಾರಿ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" स्ट्रिंग  'w' में  _right_ का उपस्थिति अधिकतम  _left_ बार है "
+				 },
+		"atLeast":{
+					"english" :" the occurance of _right_ in string 'w' is atleast _left_ time's ",
+					"kannada" :" 'w' ನಲ್ಲಿ _right_ ದರ ಸಂಭವ ಕನಿಷ್ಠಪಕ್ಷ _left_ ಬಾರಿ ",
+					"hindi":" स्ट्रिंग  'w' में  _right_ का उपस्थिति न्यूनतम _left_ बार  है "
+	              },
+		"length":{
+					"english" :" length of string 'w' ",
+					"kannada" :" 'w'ವಿನಾ ಉದ್ದ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" स्ट्रिंग 'w' की लम्बाई "
+	             },
+		"subString":{
+					"english" :" the characters from position _left_ upto but not including position _right_ in string 'w' ",
+					"kannada" :" 'w' ನಲ್ಲಿ _left_ನೇ ಸ್ಥಾನದಿಂದ _right_ನೇ ಸ್ಥಾನದವರಗಿಂತ ಮುಂಚೆ ಬರುವ ಅಕ್ಷರಗಳು ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" स्ट्रिंग  'w' में संख्या _left_ स्थान से _right_ स्थान के पहले तक आने वाले अक्षरो "
+					},
+		"isOdd":{
+					"english" :" _left_ is odd ",
+					"kannada" :" _left_ ಬೆಸ ಸಂಖ್ಯೆ ಆಗಿರುತ್ತದೆ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" _left_ विषम संख्या है "
+				},
+		"isEven":{
+					"english" :" _left_ is even ",
+					"kannada" :" _left_ ಸಮ ಸಂಖ್ಯೆ ಆಗಿರುತ್ತದೆ ",
+					"kanglish":"",
+					"tamil"   :"",
+					"hindi"   :" _left_ सम संख्या है "
+				 },
+		"&": {
+			"english": " (_left_) and (_right_) ",
+			"kannada": " (_left_) ಮತ್ತು (_right_) ",
+			"kanglish": "",
+			"tamil"   :"",
+			"hindi"   :" (_left_) और   (_right_) "
+			 },
+		"|": {
+			"english": " (_left_) or (_right_) ",
+			"kannada": " (_left_) ಅಥವಾ  (_right_) ",
+			"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" (_left_) या  (_right_) "
+			 },
+		"!": {
+			"english": " not true that (_left_) ",
+			"kannada": " ಮುಂಡಿರುವ ಸಂದರ್ಭವನ್ನು ಹೊಂದಿರುವುದಿಲ್ಲ (_left_) ",
+			"kanglish": "",
+				"tamil"   :"",
+				"hindi"   :" (_left_) यह संदर्भ स्ट्रिंग  'w' मे शामिल नहीं होगा "
+			 },
+		"!=": {
+			"english": " (_left_) does not equal to (_right_) ",
+			"kannada": " (_left_) ಮತ್ತು (_right_) ಸಮವಾಗಿರುವುದಿಲ್ಲ ",
+			"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" (_left_) और (_right_) बराबर नहीं है "
+			  },
+		"==": {
+			"english": " (_left_) equals to (_right_) ",
+			"kannada": " (_left_) ಮತ್ತು (_right_) ಸಮವಾಗಿರುತದೆ ",
+			"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" (_left_) और (_right_) बराबर है "
+			  },
+		"<=": {
+			"english": " (_left_) is greater than or equal to (_right_) ",
+			"kannada": " (_left_) ಮತ್ತು (_right_) ಹೆಚ್ಚು ಅಥವಾ  ಸಮವಾಗಿರುತದೆ ",
+			"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" (_left_) (_right_) से ज़्यादा या बराबर है "
+			  },
+		">=": {
+			"english": " (_left_) is lesser than or equal to (_right_) ",
+			"kannada": " (_left_) ಮತ್ತು (_right_) ಕಡಿಮೆ ಅಥವಾ  ಸಮವಾಗಿರುತದೆ ",
+			"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" (_left_) (_right_) से कम या बराबर है "
+			  },
+		">": {
+			"english": " (_left_) is greater than (_right_) ",
+			"kannada": " (_left_) (_right_) ಗಿಂತ ಹೆಚ್ಚಾಗಿರುತದೆ ",
+			"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" (_left_) (_right_) से ज़्यादा है "
+			  },
+		"<": {
+			"english": " (_left_) is lesser than (_right_) ",
+			"kannada": " (_left_) (_right_) ಗಿಂತ ಕಡಿಮೆಯಾಗಿರುತದೆ ",
+			"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" (_left_) (_right_) से कम है "
+			  },	 
+		"+": {
+			"english": " (_left_) on addition with (_right_) ",
+			"kannada": " (_left_) ಮತ್ತು (_right_) ಸೇರಿಸಿದಾಗ ",
+			"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" (_left_) और (_right_) को जोड़ने पर "
+			  },
+		"-": {
+			"english": " (_right_) subtracted from (_left_) ",
+			"kannada": " (_right_) ವನ್ನು (_left_) ರಿಂದ ಕಳೆದಾಗ ",
+			"kanglish": "",
+					"tamil"   :"",
+					"hindi"   :" (_right_) को (_left_) से कम करने पर "
+			 }
+	},
+	general:function(e,arr,i,lang){//bw,ew,contains,atpos,fromend,num,instr
+				var str = "";
+				var j = (2*i) + 1;
+				var r = j + 1;
+				var k = (2*j) + 1;
+				var l = k + 1;
+				//alert(e+" "+i+" "+j+" "+r+" "+k+" "+l);
+				
+					//alert("infor");
+				if(lang == 'kannada')
+				{
+					str += this.phrases[e].kannada;
+				}
+				else if(lang == 'english')
+				{
+					str += this.phrases[e].english;
+				}
+				else if(lang == 'hindi')
+				{
+					str += this.phrases[e].hindi;
+				}
+				
+				if(str.indexOf("_left_") > -1 )
+				{
+					if(this.phrases[arr[j]])
+					{
+						str = str.replace("_left_", this.general(arr[j],arr,j,lang));
+					}
+					else if(this.phrases[arr[k]])
+					{
+						str = str.replace("_left_", this.general(arr[k],arr,k,lang));
+					}
+					else if(arr[k] != "none" )
+					{
+						str = str.replace("_left_", arr[k]);
+					}
+					else if(arr[j] != "none" )
+					{
+						str = str.replace("_left_", arr[j]);
+					}
+				}
+				if(str.indexOf("_right_") > -1)
+				{
+					if(this.phrases[arr[r]])
+					{
+						str = str.replace("_right_", this.general(arr[r],arr,r,lang));
+					}
+					else if(this.phrases[arr[l]])
+					{
+						str = str.replace("_right_", this.general(arr[l],arr,l,lang));
+					}
+					else if(arr[l] != "none" )
+					{
+						str = str.replace("_right_", arr[l]);
+					}
+					else if(arr[r] != "none" )
+					{
+						str = str.replace("_right_", arr[r]);
+					}
+				}
+				return str;
+		},
+	translate:function(string, lang)
+	{
+		//alert(string[i]+"  "+i);
+		//alert(string);
+		//alert(lang);
+		var ele = string[0];
+		//alert(this.phrases[ele]);
+		if(finJson.phrases[ele])
+		{
+			return finJson.general(ele,string,0,lang);
+		}
+	}
+}
